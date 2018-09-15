e8a4fea78a004107ed2dd37db3476a1a7aec92d5
Joe Knight <knightjoe2222@gmail.com>
2018-09-15 04:06:34 -0400

    added player search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  post 'players/search' => 'players#search'
+
   resources :players
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "players#index"
 end

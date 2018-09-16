b2fbbd80032478d34589d05dbda4f133cf19e2f7
Joe Knight <knightjoe2222@gmail.com>
2018-09-16 11:58:05 -0400

    got rid of motion sickness

--- public/main.js
+++ public/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-  $('.searchTerm').click(function () {
-      $(this).parent().parent().parent().animate({
-        marginTop: "30px"
-      }, 1000, function() {});
-  });
+
 
   $('#playerInputautocomplete-list div').click(function(){
     console.log("here!");
